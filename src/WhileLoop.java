2005a742f31511716e806a175a5447695df02097
Michael Pope <mpope.cr@gmail.com>
2014-12-28 15:54:59 -0500

    Add while loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while(count < 100) {
+            System.out.println(count);
+            count += 1;
+        }
+    }
+}
