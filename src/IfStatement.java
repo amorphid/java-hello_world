8140dc16e613c3f4df34daafe9871095092d380f
Michael Pope <mpope.cr@gmail.com>
2014-12-28 17:41:53 -0500

    Add if statement

---
+++ src/IfStatement.java
@@ -0,0 +1,11 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        int num = 2;
+
+        if (num == 1) {
+            System.out.println("One");
+        } else if (num == 2) {
+            System.out.println("Two");
+        }
+    }
+}
