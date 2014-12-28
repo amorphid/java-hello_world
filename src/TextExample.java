e28b769dbd239e46287d2e43271a020c5b083e94
Michael Pope <mpope.cr@gmail.com>
2014-12-28 14:38:43 -0500

    Add string + integer

---
+++ src/TextExample.java
@@ -0,0 +1,10 @@
+public class TextExample {
+    public static void main(String[] args) {
+        String foo = "I am a foo!";
+        System.out.println(foo);
+
+        String bar = "Bar equals ";
+        int num= 99;
+        System.out.println(bar + num);
+    }
+}
