069f2843e84eedfbaa688f9a78d7fe6f7f8db377
Michael Pope <mpope.cr@gmail.com>
2014-12-29 16:23:42 -0500

    Add StringArray

---
+++ src/StringArray.java
@@ -0,0 +1,15 @@
+public class StringArray {
+    public static void main(String[] args) {
+        String[] fruits = {
+                "Banana",
+                "Pear",
+                "Apple",
+                "Plum",
+                "Cherry"
+        };
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
