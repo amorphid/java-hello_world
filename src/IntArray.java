608c2b87dbbc10fe37346c2eb3201a4c9e5e5616
Michael Pope <mpope.cr@gmail.com>
2014-12-29 15:13:38 -0500

    Add IntArray

---
+++ src/IntArray.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class IntArray {
+    public static void main(String[] args) {
+        int[] array = { 7,4,3 };
+        System.out.println(Arrays.toString(array));
+    }
+}
